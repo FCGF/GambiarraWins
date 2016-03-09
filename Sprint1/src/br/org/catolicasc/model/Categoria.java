15eecd96e738d52dfbcbf9b0657be1d476368797
Cyber <Cyber@10.197.34.152>
2016-03-09 20:26:47 -0300

    implementado categoria

---
+++ Sprint1/src/br/org/catolicasc/model/Categoria.java
@@ -0,0 +1,50 @@
+
+package br.org.catolicasc.model;
+
+
+public class Categoria {
+    
+    private int id;
+    private String nome;
+    private String descricao;
+
+    public Categoria() {
+        super();
+    }
+
+    public Categoria(int id, String nome, String descricao) {
+        this();
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nome=" + nome + ", descricao=" + descricao + '}';
+    } 
+}
