e72afb3af3910b10d60e32e15b3b7607ae100145
rafael.chewinski <rafael.chewinski@LAB0605.catolicasc.org.br>
2016-03-10 19:11:00 -0300

    TesteCategoria adicionado em br.org.catolicasc.TesteConexao

---
+++ Sprint1/src/br/org/catolica/TesteConexao/TesteCategoria.java
@@ -0,0 +1,66 @@
+package br.org.catolica.TesteConexao;
+
+import br.org.catolicasc.model.Categoria;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteCategoria {
+    
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://10.197.8.203:3306/db_rafael_chewinski?useSSL=false";
+    private static final String USER = "rafael_chewinski";
+    private static final String PASSWORD = "abc123";
+    private static final String SQL = "select * from categoria";
+    
+    public static void main(String[] args) throws ClassNotFoundException {        
+        Connection conn = null;   
+        Statement stmt = null;
+        
+         try {           
+            Class.forName(JDBC_DRIVER);           
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            
+            stmt = conn.createStatement();
+            
+            ResultSet rs = stmt.executeQuery(SQL);
+        
+            List<Categoria> categorias = new ArrayList<>();
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String nome = rs.getString("nome");
+                    String descricao = rs.getString("descricao");
+
+                    Categoria categoria = new Categoria (id, nome, descricao);
+                    categorias.add(categoria);
+
+                    System.out.println("ID: " + id + " -- " + " Nome: " + nome + " -- " + " Descricao: " + descricao);
+            }
+            
+        } catch (SQLException ex) {           
+            ex.printStackTrace();            
+        } finally {
+             try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException ex) {           
+                ex.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {           
+                ex.printStackTrace();
+            }
+        }
+    }
+}       
+    
+   
+
