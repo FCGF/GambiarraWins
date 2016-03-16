0f926d05c3610529b00d0d56587ef90a46809b91
Cyber <Cyber@10.197.34.152>
2016-03-16 13:55:58 -0300

    Atualizado testes v v
    Atualizado teste de categoriaJdbcTest.java
    Atualizado teste de produtoJdbcTest.java(!!!!!Não terminado!!!!!).
    
    Criado test de Entity.java(Mas nao feito).
    Criado test de Produto.java(Mas nao feito).

---
+++ Sprint1/test/br/org/catolicasc/cadastro/model/EntityTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.catolicasc.cadastro.model;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cyber
+ */
+public class EntityTest {
+    
+    public EntityTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @Test
+    public void testGetId() {
+    }
+
+    @Test
+    public void testSetId() {
+    }
+
+    public class EntityImpl extends Entity {
+    }
+    
+}
---
+++ Sprint1/test/br/org/catolicasc/cadastro/model/ProdutoTest.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.catolicasc.cadastro.model;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cyber
+ */
+public class ProdutoTest {
+    
+    public ProdutoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @Test
+    public void testGetCategoria() {
+    }
+
+    @Test
+    public void testSetCategoria() {
+    }
+
+    @Test
+    public void testGetNome() {
+    }
+
+    @Test
+    public void testSetNome() {
+    }
+
+    @Test
+    public void testGetPrecoUnitario() {
+    }
+
+    @Test
+    public void testSetPrecoUnitario() {
+    }
+
+    @Test
+    public void testGetQuantidade() {
+    }
+
+    @Test
+    public void testSetQuantidade() {
+    }
+
+    @Test
+    public void testGetEstoque() {
+    }
+
+    @Test
+    public void testSetEstoque() {
+    }
+
+    @Test
+    public void testIsDescontinuado() {
+    }
+
+    @Test
+    public void testDescontinua() {
+    }
+
+    @Test
+    public void testContinua() {
+    }
+    
+}
