51527c5d540924e0d33bd952af00bbfed4e6aca8
Guilherme <Guilherme@10.197.39.96>
2016-03-16 21:48:36 -0300

    Instanciada a classe Cliente

--- Sprint2.1/src/br/org/catolicasc/cadastro/model/Cliente.java
+++ Sprint2.1/src/br/org/catolicasc/cadastro/model/Cliente.java
@@ -1,43 +1,82 @@
 package br.org.catolicasc.cadastro.model;
 
+import java.util.Date;
+
 /**
  *
- * @author flavio.kannenberg
+ * @author gambiarraWins
  */
-public class Cliente extends Entity {
+public class Cliente extends Pessoa {
 
-    private String nome;
+    private double limiteCredito;
+    private double numeroCartaoCredito;
+    private String contato;
     private boolean ativo;
 
     public Cliente() {
         super();
     }
 
-    public Cliente(int id, String nome, boolean ativo) {
+    public Cliente(int id) {
         super(id);
-        this.nome = nome;
+    }
+
+    public Cliente(int id, String nome, Date dataNascimento, boolean ativo) {
+        super(nome, dataNascimento, id);
         this.ativo = ativo;
     }
 
-    public String getNome() {
-        return nome;
+    public double getLimiteCredito() {
+        return limiteCredito;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public double getNumeroCartaoCredito() {
+        return numeroCartaoCredito;
+    }
+
+    public String getContato() {
+        return contato;
     }
 
     public boolean isAtivo() {
         return ativo;
     }
 
+    public void setLimiteCredito(double limiteCredito) {
+        this.limiteCredito = limiteCredito;
+    }
+
+    public void setNumeroCartaoCredito(double numeroCartaoCredito) {
+        this.numeroCartaoCredito = numeroCartaoCredito;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
+    }
+    
+    public boolean verificaCredito(double valorCompra){
+        return (valorCompra <= limiteCredito);
+    }
+    
+    public boolean validaCartao(){
+        return (getNumeroCartaoCredito()>0);
     }
 
     @Override
     public String toString() {
-        return String.format("Cliente=[ ID: %d, Nome: %s, Ativo: %b ]", getId(), nome, ativo);
+        return String.format("Cliente=[ ID: %d, Nome: %s, Ativo: %b, "
+                + "LimiteCredito: %f, Numero Cartao Credito: %f, Contato: %s ]",
+                getId(), 
+                getNome(), 
+                ativo, 
+                getLimiteCredito(),
+                getNumeroCartaoCredito(),
+                getContato()
+        );
     }
 
 }
