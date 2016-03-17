c1b657566cd094f2367734615ad0366756d14584
FCGF <fernando.garcia@catolicasc.org.br>
2016-03-17 19:14:20 -0300

    Sprint 2. Adicionado varivalor valorKilo, e seus métodos relacionados.

--- Sprint2.1/src/br/org/catolicasc/cadastro/model/UnidadePeso.java
+++ Sprint2.1/src/br/org/catolicasc/cadastro/model/UnidadePeso.java
@@ -5,9 +5,23 @@ package br.org.catolicasc.cadastro.model;
  * @author GambiarraWins
  */
 public enum UnidadePeso {
-    QUILOGRAMA,
-    GRAMA,
-    TONELADA;
+    QUILOGRAMA(1.0),
+    GRAMA(1000.0),
+    TONELADA(0.1);
+
+    private UnidadePeso(double valorKilo) {
+        this.valorKilo = valorKilo;
+    }
+
+    private final double valorKilo;
+
+    protected double getFator() {
+        return valorKilo;
+    }
+
+    public double converteAKilo(UnidadePeso unidade, double valor) {
+        return valor / unidade.getFator();
+    }
 
     public static UnidadePeso parse(int indice) {
         UnidadePeso unidade = null;
