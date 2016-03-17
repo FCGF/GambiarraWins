84fe31fb839aa6e89f960eb0ae3419e92f849b49
FCGF <fernando.garcia@catolicasc.org.br>
2016-03-16 22:00:10 -0300

    Sprint 2. Adicionado método parse.

--- Sprint2.1/src/br/org/catolicasc/cadastro/model/UnidadePeso.java
+++ Sprint2.1/src/br/org/catolicasc/cadastro/model/UnidadePeso.java
@@ -8,4 +8,20 @@ public enum UnidadePeso {
     QUILOGRAMA,
     GRAMA,
     TONELADA;
+
+    public static UnidadePeso parse(int indice) {
+        UnidadePeso unidade = null;
+        switch (indice) {
+            case 0:
+                unidade = QUILOGRAMA;
+                break;
+            case 1:
+                unidade = GRAMA;
+                break;
+            case 2:
+                unidade = TONELADA;
+                break;
+        }
+        return unidade;
+    }
 }
