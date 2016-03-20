89dbf785cf3919272b4f23784c218c9d1493f1ee
FCGF <fernando.garcia@catolicasc.org.br>
2016-03-20 19:57:04 -0300

    Sprint 2. Métodos para teste criados.

--- Sprint2.1/test/br/org/catolicasc/cadastro/model/PedidoTest.java
+++ Sprint2.1/test/br/org/catolicasc/cadastro/model/PedidoTest.java
@@ -2,7 +2,6 @@ package br.org.catolicasc.cadastro.model;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,7 +21,7 @@ public class PedidoTest {
     @Before
     public void setUp() {
         final Calendar cal = Calendar.getInstance();
-        Date data = cal.getTime();
+        final Date data = cal.getTime();
         instance = new Pedido(data, 10);
     }
 
@@ -34,75 +33,94 @@ public class PedidoTest {
     @Test
     public void testSetVendedor() {
         System.out.println("setVendedor");
-        String vendedor = "";
-        Pedido instance = new Pedido();
+        String vendedor = "vendedor";
         instance.setVendedor(vendedor);
-        fail("The test case is a prototype.");
+        assertEquals(vendedor, instance.getVendedor());
     }
 
     @Test
     public void testSetObservacao() {
         System.out.println("setObservacao");
-        String observacao = "";
-        Pedido instance = new Pedido();
+        String observacao = "observacao";
         instance.setObservacao(observacao);
-        fail("The test case is a prototype.");
+        assertEquals(observacao, instance.getObservacao());
     }
 
     @Test
     public void testSetCliente() {
         System.out.println("setCliente");
-        Cliente cliente = null;
-        Pedido instance = new Pedido();
+        final Calendar cal = Calendar.getInstance();
+        final Date data = cal.getTime();
+        final int id = 5;
+        final String nome = "teste";
+        final boolean ativo = false;
+        Cliente cliente = new Cliente(id, nome, data, ativo);
         instance.setCliente(cliente);
-        fail("The test case is a prototype.");
+        assertEquals(id, instance.getCliente().getId());
+        assertEquals(nome, instance.getCliente().getNome());
+        assertFalse(instance.getCliente().isAtivo());
     }
 
     @Test
     public void testSetData() {
         System.out.println("setData");
-        Date data = null;
-        Pedido instance = new Pedido();
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -20);
+        Date data = cal.getTime();
         instance.setData(data);
-        fail("The test case is a prototype.");
+        Date resultado = instance.getData();
+        assertEquals(resultado, data);
     }
 
     @Test
     public void testSetItens() {
         System.out.println("setItens");
-        List<ItemPedido> itens = null;
-        Pedido instance = new Pedido();
-        instance.setItens(itens);
-        fail("The test case is a prototype.");
+        final int id = 7;
+        final int quantidade = 3;
+        final double preco = 3.14;
+
+        Produto produto = new Produto(6, "teste");
+        ItemPedido item = new ItemPedido(quantidade, preco, produto, id);
+        instance.getItens().add(item);
+        assertEquals(id, instance.getItens().get(0).getId());
+        assertEquals(quantidade, instance.getItens().get(0).getQuantidade());
+        assertEquals(preco, instance.getItens().get(0).getPreco(), 0);
     }
 
     @Test
     public void testSetStatusPedido() {
         System.out.println("setStatusPedido");
-        StatusPedido statusPedido = null;
-        Pedido instance = new Pedido();
+        StatusPedido statusPedido = StatusPedido.ENVIADO;
         instance.setStatusPedido(statusPedido);
-        fail("The test case is a prototype.");
+        StatusPedido resultado = instance.getStatusPedido();
+        assertEquals(statusPedido.ordinal(), resultado.ordinal());
     }
 
     @Test
     public void testCalcularPreco() {
         System.out.println("calcularPreco");
-        Pedido instance = new Pedido();
-        double expResult = 0.0;
+        instance.getItens().add(new ItemPedido(3, 10, null, 0));
+        instance.getItens().add(new ItemPedido(4, 5, null, 1));
+        instance.getItens().add(new ItemPedido(5, 6, null, 2));
+        instance.getItens().add(new ItemPedido(2, 3.5, null, 3));
+        double expResult = 87.0;
         double result = instance.calcularPreco();
         assertEquals(expResult, result, 0.0);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testCalcularPeso() {
         System.out.println("calcularPeso");
-        Pedido instance = new Pedido();
-        double expResult = 0.0;
+        Produto produto = new Produto(1, "Teste");
+        produto.setPeso(10);
+        produto.setUnidade(UnidadePeso.TONELADA);
+        instance.getItens().add(new ItemPedido(3, 10, produto, 0));
+        instance.getItens().add(new ItemPedido(4, 5, produto, 1));
+        instance.getItens().add(new ItemPedido(5, 6, produto, 2));
+        instance.getItens().add(new ItemPedido(2, 3.5, produto, 3));
+        double expResult = 140000;
         double result = instance.calcularPeso();
         assertEquals(expResult, result, 0.0);
-        fail("The test case is a prototype.");
     }
 
 }
