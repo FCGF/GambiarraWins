9f9745e342dac35e2ed862b36ec7f42548db5bb0
igorfachini <igor.fachini@unerj.br>
2016-05-11 11:25:53 -0300

    Adicionado exemplosDiagrama de classes(Astah) e projeto JAVA sobre BUILDER

---
+++ Sprint3/Builder/Builder/src/br/org/catolicasc/builder/VendaCarros/CarroProduct.java
@@ -0,0 +1,9 @@
+package br.org.catolicasc.builder.VendaCarros;
+
+public class CarroProduct {
+  public double preco;
+  public String dscMotor;
+  public int anoDeFabricacao;
+  public String modelo;
+  public String montadora;
+}
