4d5d08e307944540a363aab85fd1ada0656fab7f
igorfachini <igor.fachini@unerj.br>
2016-05-11 21:53:57 -0300

    Atualizado nome do pacote

---
+++ Sprint3/Codigo/Sprint3/src/br/org/catolicasc/criacao/builder/vendaCarros/CarroProduct.java
@@ -0,0 +1,9 @@
+package br.org.catolicasc.criacao.builder.vendaCarros;
+
+public class CarroProduct {
+  public double preco;
+  public String dscMotor;
+  public int anoDeFabricacao;
+  public String modelo;
+  public String montadora;
+}
