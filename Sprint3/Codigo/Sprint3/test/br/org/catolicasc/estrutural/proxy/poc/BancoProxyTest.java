93ae6a7999707e8f1a729e8cd207bc448bd4cb86
admin <admin@Joe.home>
2016-05-15 20:18:51 -0300

    O pacote test proxy poc esta em filalização

---
+++ Sprint3/Codigo/Sprint3/test/br/org/catolicasc/estrutural/proxy/poc/BancoProxyTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.catolicasc.estrutural.proxy.poc;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class BancoProxyTest {
+    
+    public BancoProxyTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNumeroDeUsuarios method, of class BancoProxy.
+     */
+    @Test
+    public void testGetNumeroDeUsuarios() {
+        System.out.println("getNumeroDeUsuarios");
+        BancoProxy instance = null;
+        String expResult = "";
+        String result = instance.getNumeroDeUsuarios();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getUsuariosConectados method, of class BancoProxy.
+     */
+    @Test
+    public void testGetUsuariosConectados() {
+        System.out.println("getUsuariosConectados");
+        BancoProxy instance = null;
+        String expResult = "";
+        String result = instance.getUsuariosConectados();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
