28219dca88304f02d5b2abbce228a5555fcddabc
Administrador <Administrador@10.197.10.26>
2016-05-16 18:45:58 -0300

    em finalizasacao

--- Sprint3/Codigo/Sprint3/src/br/org/catolicasc/estrutural/proxy/modelo/RealSubject.java
+++ Sprint3/Codigo/Sprint3/src/br/org/catolicasc/estrutural/proxy/modelo/RealSubject.java
@@ -2,24 +2,29 @@ package br.org.catolicasc.estrutural.proxy.modelo;
 
 public class RealSubject implements Arquivo {
 
-    private String nomeDoArquivo;
+    private  String nomeDoArquivo;
 
-    RealSubject(String nomeDoArquivo) {
+   public  RealSubject(String nomeDoArquivo) {
         this.nomeDoArquivo = nomeDoArquivo;
     }
 
- 
     public void Request() {
         System.out.println("Carregando Arquivo");
     }
 
     public String arquivoReal(String real) {
-        return real = nomeDoArquivo;
+
+        if (real.equals(nomeDoArquivo)) {
+            return nomeDoArquivo;
+        } else {
+            return null;
+        }
+
     }
 
     @Override
     public void getArquivos() {
-      System.out.println(this.nomeDoArquivo);
+        System.out.println(this.nomeDoArquivo);
     }
 
 }
