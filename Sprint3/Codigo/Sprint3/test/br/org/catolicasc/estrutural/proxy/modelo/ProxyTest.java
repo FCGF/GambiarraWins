87809ab0412826b5c20129c36baf3bea3a0f9a7e
Administrador <Administrador@10.197.10.26>
2016-05-16 18:16:22 -0300

    Pacote modelo Test proxy finalizado

---
+++ Sprint3/Codigo/Sprint3/test/br/org/catolicasc/estrutural/proxy/modelo/ProxyTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.catolicasc.estrutural.proxy.modelo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class ProxyTest {
+
+    /**
+     * Test of RequestProxy method, of class Proxy.
+     */
+    @Test
+    public void testRequestProxy() {
+        System.out.println("RequestProxy");
+        String nomeDoArquivo = "Nome";
+        Proxy instance = new Proxy("Nome");
+        instance.RequestProxy(nomeDoArquivo);
+        assertNotNull(nomeDoArquivo);
+        assertFalse(nomeDoArquivo.isEmpty());
+
+    }
+
+    /**
+     * Test of getArquivos method, of class Proxy.
+     */
+    @Test
+    public void testGetArquivos() {
+        System.out.println("getArquivos");
+        Proxy instance = new Proxy("Nome");
+        String nomeDoArquivo = "Nome";
+        assertNotNull(nomeDoArquivo);
+        assertFalse(nomeDoArquivo.isEmpty());
+    }
+
+}
---
+++ Sprint3/Codigo/Sprint3/test/br/org/catolicasc/estrutural/proxy/modelo/RealSubjectTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.catolicasc.estrutural.proxy.modelo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class RealSubjectTest {
+
+    /**
+     * Test of arquivoReal method, of class RealSubject.
+     */
+    @Test
+    public void testArquivoReal() {
+        String real = "Nome";
+        System.out.println("arquivoReal");
+        RealSubject instance = new RealSubject(real);
+        String result = instance.arquivoReal(real);
+        assertEquals(real, result);
+
+    }
+
+    public void testArquivoRealErrado() {
+        System.out.println("ArquivoRealErrado");
+        String real = "Nome";
+        RealSubject instance = new RealSubject("Gambiarra");
+        String result = instance.arquivoReal(real);
+        assertEquals(real, result);
+
+    }
+
+}
